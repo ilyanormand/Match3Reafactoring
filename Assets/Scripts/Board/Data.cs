47aa18bdc681f86043605d6d5004800bf5b532c4
ilia <ilya@1-tn.com>
2020-09-24 22:42:33 +0200

    made fruit generation

---
+++ Assets/Scripts/Board/Data.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Data : MonoBehaviour
+{
+    public List<int> FruitArray;
+    public bool addedToArray = false;
+    private void Awake()
+    {
+        FruitArray = new List<int>();
+    }
+    void Start()
+    {
+
+    }
+
+    private void WriteIntoFile(string path) 
+    {
+        StreamWriter sw = new StreamWriter(path + "/fruitArrayData.txt");
+        for (int i = 0; i < FruitArray.Count; i++) 
+        {
+            sw.WriteLine(FruitArray[i]);
+        }
+        sw.Close();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (addedToArray) 
+        {
+            WriteIntoFile(Application.dataPath);
+            addedToArray = false;
+        }*/
+        
+    }
+}
+
---
+++ Assets/Scripts/Fruit/FruitGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FruitGenerator : MonoBehaviour
+{
+    public GameObject fruitPrefab;
+    public Data data;
+    public int[] TypeOfFruit;
+    void Start()
+    {
+        data = FindObjectOfType<Data>();
+        GameObject fruit = Instantiate(fruitPrefab, transform.position, Quaternion.identity);
+        fruit.transform.parent = this.transform;
+        int index = Random.Range(0, TypeOfFruit.Length);
+        fruit.transform.GetChild(TypeOfFruit[index]).gameObject.SetActive(true);
+        Debug.Log(data.FruitArray);
+        data.FruitArray.Add(TypeOfFruit[index]);
+        data.addedToArray = true;
+    }
+}
+
